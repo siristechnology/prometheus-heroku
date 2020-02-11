2152dbaf59f46e3ab640050d59531a7f6a2ee0e6
Lutogin <a.lutogin@artjoker.team>
2020-02-11 16:35:53 +0200

    add fixes

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,13 +3,10 @@
 # we use an entrypoint script here with PORT setting so that
 # we can run this on heroku using containers
 export SERVER_PORT="${PORT:-8080}"
-echo "Arguments are $*"
-echo "Working with ${SERVER_PORT}"
 
 #
 # This is the probot host that is collecting our metrics
-echo "Configure PROBOT_HOST => ${PROBOT_HOST}"
-sed -i "s/{{PROBOT_HOST}}/${PROBOT_HOST}/g" /etc/prometheus/prometheus.yml
+sed -i "s/{{CHARGO_HOST}}/${CHARGO_HOST}/g" /etc/prometheus/prometheus.yml
 
 echo "Starting up server"
 "/bin/prometheus" \
