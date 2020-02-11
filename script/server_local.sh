3b728c70ba50e2233b977dd7dfe25fa664ebca5f
Lutogin <a.lutogin@artjoker.team>
2020-02-11 17:04:58 +0200

    add fixes 2

--- script/server_local.sh
+++ script/server_local.sh
@@ -4,8 +4,8 @@ set -e
 
 # get probot scrap url
 # curl http://${LOCAL_HOST_SCRAP}:3000/probot/metrics
-export PROBOT_HOST=$(ifconfig en0|grep 'inet\s'|awk '{print $2}')
-docker run -it -e PROBOT_HOST=${PROBOT_HOST} \
+export CHARGO_HOST=$(ifconfig en0|grep 'inet\s'|awk '{print $2}')
+docker run -it -e CHARGO_HOST=${CHARGO_HOST} \
            -e PORT=8080 \
            -p 9090:8080 \
             build-prometheus
